484a9b5adfb3cfa72280b16fad7e33373f49606e
mengkang.zmk <mengkang.zmk@alibaba-inc.com>
2019-03-02 18:44:31 +0800

    update

--- 01/7.php
+++ 01/7.php
@@ -5,40 +5,6 @@
  * Date: 2019/3/2 下午5:35
  */
 
-/**
- * @method mixed fetchList(string $sql, array $argv);
- */
-class Model
-{
-
-    public function __construct($table)
-    {
-
-    }
-}
-
-function getUserList($startId, $lastId, $limit = 100)
-{
-    if ($lastId > 0) {
-        $startId = $lastId;
-    }
-
-    $sql = "select * from `user` where id > ? order by id asc limit ?,?";
-
-    $model = new Model('user');
-    return $model->fetchList($sql, [intval($startId), intval($limit)]);
-}
-
-
-function bad($input1, $input2, &$input3)
-{
-    //...logic
-
-    $input3 = "xxx";
-
-    return true;
-}
-
 function addUser($nickname)
 {
     if (wordsCheck($nickname)) {
